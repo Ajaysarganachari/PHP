8dc02f791f1daf8a85506a87cc9686215adc1df7
Ajay Sarganachari <ajaysaraganachari15@gmail.com>
2025-02-27 11:39:53 +0530

    Create index.php

---
+++ DAY_01/welcome_01/index.php
@@ -0,0 +1,14 @@
+<?php
+echo "
+<html>
+<head>
+<title>welcome to while loop</title>
+</head>
+<body>
+    <h1>welcome to php.</h1>
+    <h1>where are you from sir</h1>
+    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem veniam possimus architecto dolor ullam dolorem alias aspernatur? Facere, mollitia ea incidunt aperiam molestias exercitationem quaerat alias vitae provident recusandae unde?</h2>
+</body>
+</html>
+"
+?>
