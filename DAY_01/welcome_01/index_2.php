f06ad7f9f7910b593a8347d4d99e83bf5d591e9d
Ajay Sarganachari <ajaysaraganachari15@gmail.com>
2025-02-27 11:39:20 +0530

    Create index_2.php

---
+++ DAY_01/welcome_01/index_2.php
@@ -0,0 +1,25 @@
+<?php
+ echo "
+<html>
+<head>
+<title>welcome web page</title>
+</head>
+<body>
+    <h1>welcome to php</h1><br>
+    <i>italic php</i><br>
+    <b>bold php</b><br>
+    <u>underline php</u><br>
+    <em>emphasized php</em><br>
+    <strong>impotant php</strong><br>
+    <mark>marked php</mark><br>
+    <small>saller php</small><br>
+    <big>bigger php</big><br>
+    <ins>insded php</ins><br>
+    <del>deleted php</del><br>
+    <sup>supersript php</sup><br>
+    <sub>subscript php</sub><br>
+
+</body>
+</html>
+"
+?>
